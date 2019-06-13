392afad07fd95ba8e61fbbfcce40af2f1820ae3b
Chris Chu <chrischu@kkbox.com>
2019-06-13 12:30:50 +0800

    Update index and related test case

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+export { default as _ } from './_';
 export { default as compose } from './compose';
+export { default as curry } from './curry';
 export { default as has } from './has';
 export { default as pipe } from './pipe';
 export { default as reverse } from './reverse';
